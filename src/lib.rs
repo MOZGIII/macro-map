e40616c332d04e3454c621e5196db9ecb1e67202
MOZGIII <mike-n@narod.ru>
2023-01-01 20:03:29 +0400

    Add more implementations

--- src/lib.rs
+++ src/lib.rs
@@ -89,3 +89,129 @@ macro_rules! ok_or_else {
         }
     };
 }
+
+/// An analog to [`Result::map_err`] followed by a `?` but without a closure.
+///
+/// ```
+/// use macro_map::try_map_err;
+///
+/// fn myfn() -> Result<(), i64> {
+///     let myresult: Result<&str, &str> = Err("hello");
+///     try_map_err!(myresult, |myerr| 123);
+///     Ok(())
+/// }
+///
+/// let mapped = myfn();
+///
+/// assert_eq!(mapped, Err(123));
+/// ```
+///
+/// Or with [`postfix-macros`](https://docs.rs/postfix-macros):
+///
+/// ```
+/// use macro_map::try_map_err;
+/// use postfix_macros::postfix_macros;
+///
+/// fn myfn() -> Result<(), i64> {
+///     postfix_macros! {
+///         let myresult: Result<&str, &str> = Err("hello");
+///         myresult.try_map_err!(|myerr| 123);
+///         Ok(())
+///     }
+/// }
+///
+///
+/// let mapped = myfn();
+///
+/// assert_eq!(mapped, Err(123));
+/// ```
+#[macro_export]
+macro_rules! try_map_err {
+    ($result:expr, |$err:pat_param| $closure:expr) => {
+        match $result {
+            Ok(val) => val,
+            Err($err) => return Err($closure.into()),
+        }
+    };
+}
+
+/// An analog to [`Result::map_err`] followed by a `?` but without a closure.
+///
+/// ```
+/// use macro_map::try_ok_or_else;
+///
+/// fn myfn() -> Result<(), i64> {
+///     let myoption: Option<&str> = None;
+///     try_ok_or_else!(myoption, || 123);
+///     Ok(())
+/// }
+///
+/// let mapped = myfn();
+///
+/// assert_eq!(mapped, Err(123));
+/// ```
+///
+/// Or with [`postfix-macros`](https://docs.rs/postfix-macros):
+///
+/// ```
+/// use macro_map::try_ok_or_else;
+/// use postfix_macros::postfix_macros;
+///
+/// fn myfn() -> Result<(), i64> {
+///     postfix_macros! {
+///         let myoption: Option<&str> = None;
+///         myoption.try_ok_or_else!(|| 123);
+///         Ok(())
+///     }
+/// }
+///
+///
+/// let mapped = myfn();
+///
+/// assert_eq!(mapped, Err(123));
+/// ```
+#[macro_export]
+macro_rules! try_ok_or_else {
+    ($result:expr, || $closure:expr) => {
+        match $result {
+            Some(val) => val,
+            None => return Err($closure.into()),
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use postfix_macros::postfix_macros;
+
+    #[derive(Debug)]
+    struct MyNonCopy;
+
+    fn result(a: MyNonCopy, cond: Result<(), ()>) -> Result<MyNonCopy, (MyNonCopy,)> {
+        postfix_macros! {
+            cond.try_map_err!(|_| (a,));
+            Ok(a)
+        }
+    }
+
+    fn option(a: MyNonCopy, cond: Option<()>) -> Result<MyNonCopy, (MyNonCopy,)> {
+        postfix_macros! {
+            cond.try_ok_or_else!(|| (a,));
+            Ok(a)
+        }
+    }
+
+    #[test]
+    fn test_result() {
+        result(MyNonCopy, Ok(())).unwrap();
+        result(MyNonCopy, Err(())).unwrap_err();
+    }
+
+    #[test]
+    fn test_option() {
+        option(MyNonCopy, Some(())).unwrap();
+        option(MyNonCopy, None).unwrap_err();
+    }
+}
